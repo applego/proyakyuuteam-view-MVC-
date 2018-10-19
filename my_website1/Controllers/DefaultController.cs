1d2aa9b3bee0e3e00c3c59ec0318dae6b86c097d
applego <applegorillappa@gmail.com>
2018-10-19 17:44:23 +0900

    jsのライブラリとMVCを使ってプロ野球チーム名を表示(途中)

---
+++ my_website1/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace my_website1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            ViewData["message"] = "Hello ASP.NET MVC World!";
+            return View();
+        }
+    }
+}
---
+++ my_website1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="my_website1.MvcApplication" Language="C#" %>
